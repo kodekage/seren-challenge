30dc8a8f57a9fa37facdb73c7fc18322b4f73d97
Prosper Opara <oparaprosper79@gmail.com>
2022-02-17 22:07:36 +0100

    chore: add user response test

---
+++ src/controllers/userResponse.controller.test.js
@@ -0,0 +1,46 @@
+/* eslint-env jest */
+const mongoose = require("../config/mongoose");
+const { mockRequest, mockResponse, jsonResponse } = require("../utils/helpers");
+const userResponseController = require("./userResponse.controller");
+
+jest.mock("../config/mongoose");
+jest.mock("../models/response.model", () => {
+  const methods = {
+    findOne: jest.fn(() => ({
+      exec: jest.fn(() => ({
+        mood: "neutral",
+        hobbies: ["football", "sleep"]
+      }))
+    })),
+    updateOne: jest.fn(),
+    save: jest.fn(),
+  };
+  return methods;
+});
+jest.mock("../config/logger", () => {
+  const logger = {
+    info(){ return  jest.fn(); },
+    error(){ return jest.fn(); },
+    debug(){ return jest.fn(); },
+    warn() { return jest.fn(); }
+  };
+  return logger;
+});
+
+describe("userResponseController", () => {
+  beforeAll(() => {
+    mongoose.mockImplementation(() => jest.fn());
+  });
+
+
+  test("Should return successfully when correct user params is passed", async() => {
+    const param = { params: { userName: "prosper" }};
+    const req = mockRequest(null, param);
+    const res = mockResponse();
+
+    await userResponseController(req, res);
+    
+    expect(res.status).toHaveBeenCalledWith(200);
+    expect(res.json).toHaveBeenCalledWith(jsonResponse);
+  });
+});
--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,4 +1,7 @@
 /* eslint-disable no-undef */
+
+const { Hobby, Mood } = require("./enums");
+
 /* eslint-disable no-useless-escape*/
 const mockResponse = () => {
   const res = {};
@@ -8,13 +11,29 @@ const mockResponse = () => {
   return res;
 };
   
-const mockRequest = (body) => {
+const mockRequest = (body = null, params = null) => {
   return {
-    body
+    body,
+    params,
   };
 };
 
 
 const moodPayload = "{\"type\":\"block_actions\",\"user\":{\"id\":\"U012CAEHVF0\",\"username\":\"oparaprosper79\",\"name\":\"oparaprosper79\",\"team_id\":\"T012HLUEF2R\"},\"api_app_id\":\"A033X9ADU0G\",\"token\":\"hMCBp2D9xVUxhuppThcB5ffb\",\"container\":{\"type\":\"message_attachment\",\"message_ts\":\"1645129661.393559\",\"attachment_id\":1,\"channel_id\":\"C033XMZN9CH\",\"is_ephemeral\":false,\"is_app_unfurl\":false},\"trigger_id\":\"3120388974517.1085708491093.17515ec9496f63507286b954f5464235\",\"team\":{\"id\":\"T012HLUEF2R\",\"domain\":\"kodekagedev\"},\"enterprise\":null,\"is_enterprise_install\":false,\"channel\":{\"id\":\"C033XMZN9CH\",\"name\":\"intro\"},\"message\":{\"bot_id\":\"B0337H7EKH9\",\"type\":\"message\",\"text\":\"Welcome. How are you doing?\",\"user\":\"U0339S88NH2\",\"ts\":\"1645129661.393559\",\"team\":\"T012HLUEF2R\",\"attachments\":[{\"id\":1,\"blocks\":[{\"type\":\"section\",\"block_id\":\"RrFp7\",\"text\":{\"type\":\"mrkdwn\",\"text\":\"Choose an option that reflects how your mood\",\"verbatim\":false},\"accessory\":{\"type\":\"static_select\",\"action_id\":\"user_mood\",\"placeholder\":{\"type\":\"plain_text\",\"text\":\"Select mood\",\"emoji\":true},\"options\":[{\"text\":{\"type\":\"plain_text\",\"text\":\"Feeling Lucky\",\"emoji\":true},\"value\":\"Feeling Lucky\"},{\"text\":{\"type\":\"plain_text\",\"text\":\"Neutral\",\"emoji\":true},\"value\":\"Neutral\"},{\"text\":{\"type\":\"plain_text\",\"text\":\"Doing Well\",\"emoji\":true},\"value\":\"Doing Well\"}]}}],\"color\":\"#f2c744\",\"fallback\":\"[no preview available]\"}]},\"state\":{\"values\":{\"RrFp7\":{\"user_mood\":{\"type\":\"static_select\",\"selected_option\":{\"text\":{\"type\":\"plain_text\",\"text\":\"Feeling Lucky\",\"emoji\":true},\"value\":\"Feeling Lucky\"}}}}},\"response_url\":\"https:\/\/hooks.slack.com\/actions\/T012HLUEF2R\/3147043235168\/cx1OeJPGnnuCvQUgaIYYzti2\",\"actions\":[{\"type\":\"static_select\",\"action_id\":\"user_mood\",\"block_id\":\"RrFp7\",\"selected_option\":{\"text\":{\"type\":\"plain_text\",\"text\":\"Feeling Lucky\",\"emoji\":true},\"value\":\"Feeling Lucky\"},\"placeholder\":{\"type\":\"plain_text\",\"text\":\"Select mood\",\"emoji\":true},\"action_ts\":\"1645129670.207376\"}]}";
 
-module.exports = { mockRequest, mockResponse, moodPayload };
+const jsonResponse = {
+  status: 200,
+  data:{
+    response: [
+      {
+        question: "How are you feeling",
+        answer: "neutral",
+      },
+      {
+        question: "What are your favorite hobbies?",
+        answer: ["football", "sleep"]
+      },
+    ]
+  }
+};
+module.exports = { mockRequest, mockResponse, moodPayload, jsonResponse };
